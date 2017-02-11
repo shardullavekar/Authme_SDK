76721cd6734734d5919c7b59a8e11189db7b13f0
Shardul <developershardul@gmail.com>
2017-02-11 15:53:27 +0530

    Removing old library

--- patternlock/src/main/java/haibison/android/pattern/server/Callback.java
+++
@@ -1,5 +0,0 @@
-package haibison.android.pattern.server;
-
-public interface Callback {
-    void onTaskExecuted(String response);
-}
--- patternlock/src/main/java/haibison/android/pattern/util/Callback.java
+++
@@ -1,8 +0,0 @@
-package haibison.android.pattern.util;
-
-/**
- * Created by shardul on 08-11-2015.
- */
-public interface Callback {
-    public void onResponseRecieved(String response);
-}
