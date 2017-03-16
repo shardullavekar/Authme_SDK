e7f330c44fe208f2b10a9d1192db1b7058110136
Shardul <developershardul@gmail.com>
2017-03-16 20:44:06 +0530

    Changes made to prod url

--- patternlock/src/main/java/io/authme/sdk/server/Config.java
+++ patternlock/src/main/java/io/authme/sdk/server/Config.java
@@ -25,11 +25,11 @@ public class Config {
     public static final int LOGIN_PATTERN = 2, SIGNUP_PATTERN = 1,
             INVALID_CONFIG = 3, RESULT_FAILED = 5, RESET_PATTERN = 6;
 
-    public static final String PROD_HOST = "http://authme.io";
-    public static final String PROD_SERVER_URL = PROD_HOST + ":3000/";
+    public static final String PROD_HOST = "https://api.authme.authme.host";
+    public static final String PROD_SERVER_URL = PROD_HOST;
 
-    public static final String SANDBOX_HOST = "";
-    public static final String SANDBOX_SERVER_URL = SANDBOX_HOST + ":3000/";
+    public static final String SANDBOX_HOST = "https://api.authme.authme.xyz";
+    public static final String SANDBOX_SERVER_URL = SANDBOX_HOST;
 
     public static final String STORED_VALUES = "STORED_VALUES";
     public static final String API_KEY = "API_KEY",
